a15dfb2749ec073449b297d9e81a464e75495ab1
Ganesh BMS <gbms@avaya.com>
2017-11-02 19:18:44 +0530

    Seed commit

---
+++ src/com/jpush/JPushClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jpush;
+
+/**
+ * @author ganeshbrio
+ *
+ */
+public class JPushClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JPushClient ..");
+	}
+
+}
